a556f5e7064ef5192201ea3330c57d77f956f2d8
Kairat Sultan <garri.sultonov@mail.ru>
2023-06-22 08:49:29 +0600

    remove celery signal tasks"

--- medtour/applications/signals.py
+++ medtour/applications/signals.py
@@ -3,12 +3,41 @@ from django.dispatch import receiver
 
 from medtour.applications.models import Application
 from medtour.applications.tasks import save_application_for_all_tours
+from medtour.guides.models import Guide
+from medtour.tours.models import Tour
+
+
+def save_application_for_all_tours(name, phone, obj_type, trip_id):
+    # TODO: https://mtour.kz/api/dashboard/applications/application/31/change/
+    if obj_type == "tours":
+        obj = Tour.objects.filter(pk=trip_id)
+        if obj.exists():
+            obj = obj.first().title
+        else:
+            obj = "TRIP"
+    else:
+        obj = Guide.objects.filter(pk=trip_id)
+        if obj.exists():
+            obj = obj.first().title
+        else:
+            obj = "TRIP"
+    import requests
+    from django.conf import settings
+    data = {
+        "chat_id": settings.APPLICATION_SEND_BOT_GROUP_ID,
+        "text": "Новая заявка: \n"
+                f"Объект: {obj}\n"
+                f"Имя: {name}\n"
+                f"Телефон: {phone}\n"
+    }
+    url = f"https://api.telegram.org/bot{settings.APPLICATION_SEND_BOT_TOKEN}/sendMessage"
+    requests.post(url=url, data=data)
 
 
 @receiver(post_save, sender=Application)
 def bulk_creating_tour_apps(sender, instance, created, **kwargs):
     if created:
-        save_application_for_all_tours.delay(name=instance.fullName,
-                                             phone=instance.phoneNumber,
-                                             obj_type=instance.type,
-                                             trip_id=instance.trip_id)
+        save_application_for_all_tours(name=instance.fullName,
+                                       phone=instance.phoneNumber,
+                                       obj_type=instance.type,
+                                       trip_id=instance.trip_id)
